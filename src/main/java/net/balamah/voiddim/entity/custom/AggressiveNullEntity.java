897eb555012b1ad412a01047d75054b4507b0716
balamah <m.balandin999@gmail.com>
2026-03-20 21:21:10 +0200

    fixed signs placement count for AggressiveNullEntity

--- src/main/java/net/balamah/voiddim/entity/custom/AggressiveNullEntity.java
+++ src/main/java/net/balamah/voiddim/entity/custom/AggressiveNullEntity.java
@@ -37,7 +37,7 @@ public class AggressiveNullEntity extends CorruptedHostileEntity {
 
 		Goal randomPlaceSignsGoal =
 			new RandomPlaceSignsGoal<AggressiveNullEntity>(
-				this, new String[]{"NULL", "NULL", "NULL", "NULL"}, 25, 6
+				this, new String[]{"NULL", "NULL", "NULL", "NULL"}, 100, 6
 			);
 
 		this.goalSelector.add(1, randomPlaceSignsGoal);
