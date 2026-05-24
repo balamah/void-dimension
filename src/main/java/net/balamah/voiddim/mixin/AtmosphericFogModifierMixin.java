663213b8e81406196dd83186cfb6ed63e27e8d78
balamah <m.balandin999@gmail.com>
2026-05-24 16:31:12 +0300

    improved shaders in the dimension

--- src/main/java/net/balamah/voiddim/mixin/AtmosphericFogModifierMixin.java
+++ src/main/java/net/balamah/voiddim/mixin/AtmosphericFogModifierMixin.java
@@ -21,7 +21,7 @@ public class AtmosphericFogModifierMixin {
 		CallbackInfo ci
 	) {
 		if (world.dimension().equals(ModDimensions.VOID_WORLD)) {
-			data.color.set(0.027f, 0.078f, 0.149f, 1.0f);
+			data.color.set(0.0f, 0.0f, 0.0f, 1.0f);
 			data.environmentalStart = 0.0f;
 			data.environmentalEnd = Math.min(viewDistance * 0.55f, 48.0f);
 			data.skyEnd = Math.min(data.skyEnd, data.environmentalEnd);
