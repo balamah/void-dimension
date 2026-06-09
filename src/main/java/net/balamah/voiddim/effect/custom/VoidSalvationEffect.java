8cad3bffeaf2d99b9d6c8c79d4877d1c59f8de39
balamah <m.balandin999@gmail.com>
2026-06-09 21:50:04 +0300

    renamed teleportPlayer(ServerPlayer, ResourceKey<Level>) to teleportEntity(LivingEntity, ResourceKey<Level>)

--- src/main/java/net/balamah/voiddim/effect/custom/VoidSalvationEffect.java
+++ src/main/java/net/balamah/voiddim/effect/custom/VoidSalvationEffect.java
@@ -1,14 +1,13 @@
 package net.balamah.voiddim.effect.custom;
 
 import net.balamah.voiddim.world.dimension.ModDimensions;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Relative;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
@@ -31,28 +30,27 @@ public class VoidSalvationEffect extends MobEffect {
 		String currentDimensionId = currentDimension.identifier().toString();
 		ResourceKey<Level> dimensionToTeleport = this.getDimensionToTeleport(currentDimensionId);
 
-		if (entity instanceof ServerPlayer player && entity.getY() < -120 &&
-			dimensionToTeleport != null
-		) {
-			this.teleportPlayer(player, dimensionToTeleport);
+		if (entity.getY() < -120 && dimensionToTeleport != null) {
+			this.teleportEntity(entity, dimensionToTeleport);
 		}
 
 		return super.applyEffectTick(world, entity, amplifier);
 	}
 
-	protected void teleportPlayer(
-		ServerPlayer player, ResourceKey<Level> dimensionToTeleport
+	protected void teleportEntity(
+		LivingEntity entity, ResourceKey<Level> dimensionToTeleport
 	) {
 		EnumSet<Relative> flags = EnumSet.of(Relative.X, Relative.Y, Relative.Z);
 		ServerLevel dimension =
-			player.level().getServer().getLevel(dimensionToTeleport);
+			entity.level().getServer().getLevel(dimensionToTeleport);
 
 		if (dimension != null) {
-			player.teleportTo(
-				dimension, 100, 250, 100, flags, player.getYRot(), player.getXRot(), true
+			entity.teleportTo(
+				dimension, entity.getX(), 250, entity.getZ(),
+				flags, entity.getYRot(), entity.getXRot(), true
 			);
 
-			player.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 350, 1));
+			entity.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 350, 1));
 		}
 	}
 
