0e4e74e731d1c259b9fc77fa5e8a0aed9126723b
balamah <m.balandin999@gmail.com>
2026-07-18 13:28:20 +0300

    added loot table links for warrior fortress structures

--- src/main/java/net/balamah/voiddim/data/loot/ModLootTables.java
+++ src/main/java/net/balamah/voiddim/data/loot/ModLootTables.java
@@ -103,6 +103,70 @@ public class ModLootTables {
 	public static ResourceKey<LootTable> SCHOLAR_BEACON_LOOT =
 		register("chests/expedition_bases/scholar_beacon_loot");
 
+	public static ResourceKey<LootTable> FORTRESS_KITCHEN_FOOD_LOOT =
+		register("fortress/kitchen/food_loot");
+
+	/*
+	 * Fortress loot table paths are wrong, but i don't give a shit.
+	 * I am tired of making this update
+	 */
+	public static ResourceKey<LootTable> FORTRESS_KITCHEN_COAL_FOOD_LOOT =
+		register("fortress/kitchen/coal_food_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_MAZE_SECRET_LOOT =
+		register("fortress/maze/secret_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_WOOL_LOOT =
+		register("fortress/wool_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_WOOL_SECRET_LOOT =
+		register("fortress/wool/secret_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_ARMORY_LOOT =
+		register("fortress/armory_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_LIBRARY_LOOT =
+		register("fortress/library_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_MUSIC_LOOT =
+		register("fortress/music_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_THIRD_FLOOR_TOWER_LOOT =
+		register("fortress/third_floor/tower_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_THIRD_FLOOR_JEWELER_CHEST_LOOT =
+		register("fortress/third_floor/jeweler_chest_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_THIRD_FLOOR_CARPENTER_CHEST_LOOT =
+		register("fortress/third_floor/carpenter_chest_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_THIRD_FLOOR_BUILDER_CHEST_LOOT =
+		register("fortress/third_floor/builder_chest_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_THIRD_FLOOR_PROVISIONER_CHEST_LOOT =
+		register("fortress/third_floor/provisioner_chest_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_THIRD_FLOOR_WEAPONER_CHEST_LOOT =
+		register("fortress/third_floor/weaponer_chest_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_THIRD_FLOOR_SECRET_CHEST_LOOT =
+		register("fortress/third_floor/secret_chest_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_THIRD_FLOOR_TENT_CHEST_LOOT =
+		register("fortress/third_floor/tent_chest_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_WARES_MATERIALS_LOOT =
+		register("fortress/wares/materials_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_WARES_WEAPONS_LOOT =
+		register("fortress/wares/weapons_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_WARES_TREASURES_LOOT =
+		register("fortress/wares/treasures_loot");
+
+	public static ResourceKey<LootTable> FORTRESS_WARES_PROVISIONS_LOOT =
+		register("fortress/wares/provisions_loot");
+
 	public static void registerModLootTables() {
 		VoidDimension.LOGGER.info("Registering mod loot tables for " + VoidDimension.MOD_ID);
 	}
