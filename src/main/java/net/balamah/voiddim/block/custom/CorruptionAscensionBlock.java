dd249d0fff0bd49f3b224d4d4ef8b969f34511b0
balamah <m.balandin999@gmail.com>
2026-07-20 12:53:17 +0300

    finished working with CorruptionAscensionEffect

--- src/main/java/net/balamah/voiddim/block/custom/CorruptionAscensionBlock.java
+++ src/main/java/net/balamah/voiddim/block/custom/CorruptionAscensionBlock.java
@@ -1,7 +1,10 @@
 package net.balamah.voiddim.block.custom;
 
+import net.balamah.voiddim.custom.McCodeHelper;
 import net.balamah.voiddim.effect.ModEffects;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -15,12 +18,21 @@ public class CorruptionAscensionBlock extends CorruptBlock {
 
 	@Override
 	public void stepOn(Level world, BlockPos pos, BlockState state, Entity entity) {
-		if (entity instanceof LivingEntity livingEntity) {
-			MobEffectInstance effect = new MobEffectInstance(
-				ModEffects.CORRUPTION_ASCENSION, 60, 1
-			);
-
-			livingEntity.addEffect(effect);
+		if (!(entity instanceof LivingEntity livingEntity)) {
+			return;
 		}
+
+		Holder<MobEffect> effect;
+		boolean isAscendable = McCodeHelper.entityCorruptionAscensionMap.containsKey(
+			livingEntity.getType()
+		);
+
+		if (isAscendable) {
+			effect = ModEffects.CORRUPTION_ASCENSION;
+		} else {
+			effect = ModEffects.CORRUPTION;
+		}
+		
+		livingEntity.addEffect(new MobEffectInstance(effect, 60, 1));
 	}
 }
