475390c1f8e92728a60889ffeec54d3309467889
balamah <m.balandin999@gmail.com>
2026-09-06 13:22:47 +0300

    resized anomaly entity

--- src/main/java/net/balamah/voiddim/entity/client/MashaRenderer.java
+++ src/main/java/net/balamah/voiddim/entity/client/MashaRenderer.java
@@ -1,9 +1,13 @@
 package net.balamah.voiddim.entity.client;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+
 import net.balamah.voiddim.VoidDimension;
 import net.balamah.voiddim.entity.client.base.BasicRenderState;
+import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.state.level.CameraRenderState;
 import net.minecraft.resources.Identifier;
 import net.balamah.voiddim.entity.custom.MashaEntity;
 
@@ -22,5 +26,22 @@ public class MashaRenderer
 	@Override
 	public Identifier getTextureLocation(BasicRenderState state) {
 		return Identifier.fromNamespaceAndPath(VoidDimension.MOD_ID, "textures/entity/masha.png");
+	}
+
+	@Override
+	public void submit(
+		BasicRenderState state, PoseStack poseStack, SubmitNodeCollector submitNodeCollector,
+		CameraRenderState camera
+	) {
+		float size = 0f;
+		if (state.isBaby) {
+			size = 0.5f;
+		} else {
+			size = 0.7f;
+		}
+
+		poseStack.scale(size, size, size);
+
+		super.submit(state, poseStack, submitNodeCollector, camera);
 	}
 }
